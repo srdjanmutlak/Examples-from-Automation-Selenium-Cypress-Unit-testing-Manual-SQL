497bbc7a32d0f3f79b9779ae4a47100783d1f046
srdjanmutlak <84793610+srdjanmutlak@users.noreply.github.com>
2022-06-04 13:13:37 +0200

    Add files via upload

---
+++ Automation assignment (Cypress)/cypress/integration/noviTestovi/duplicatesAndUniques.spec.js
@@ -0,0 +1,18 @@
+describe('Testing unique and duplicate items', () => {
+    it('Find Duplicate Text Lightweight And Confirm The Rest Of Elements Are Unique', () => {
+		
+		cy.visit('https://cdpn.io/pen/debug/wvyXVRa?authentication_hash=bYMdyOpxVVQr')      
+
+        cy.get('li:contains(Lightweight)').should('have.length', 2)     
+
+		cy.get('li:not(:contains(Lightweight))')
+		.should('have.length', 3) 	
+		.then($el) => Cypress._.map($el, 'innerText'))
+		.then((values) => {
+			const distinct = Cypress._.uniq(values)
+			expect(distinct).to.have.length(values.length)
+		})
+
+    })  
+
+})
